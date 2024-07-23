e9dc5f4cf6cedf8b54cfb48bf9b33e24a78e5d30
riken-sajal <riken@sajaltech.com>
2024-07-23 16:12:26 +0530

    added headless mode

--- driver/driver.py
+++ driver/driver.py
@@ -23,6 +23,7 @@ class Driver_class():
 
     def driver_args(self):
         self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--headless")
         self.options.add_argument("--no-sandbox")
         self.options.add_argument("--disable-dev-shm-usage")
         self.options.add_argument("--disable-gpu")
