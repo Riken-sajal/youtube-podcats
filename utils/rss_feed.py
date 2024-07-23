2f66c56d06abf84aa883af290bfff18e83987641
riken-sajal <riken@sajaltech.com>
2024-07-23 21:40:15 +0530

    changed in feeds

--- utils/rss_feed.py
+++ utils/rss_feed.py
@@ -1,4 +1,5 @@
 from feedgen.feed import FeedGenerator
+import os
 
 def create_rss_feed(audio_files, base_url):
     fg = FeedGenerator()
@@ -13,10 +14,12 @@ def create_rss_feed(audio_files, base_url):
         fe = fg.add_entry()
         fe.id(base_url + audio_file.file.url)
         fe.title(audio_file.title)
-        fe.description(f'Audio file: {audio_file.title}')
-        fe.enclosure(base_url + audio_file.file.url, 0, 'audio/mpeg')
-        fe.pubDate(audio_file.uploaded_at)
-        fe.link(href=base_url + audio_file.file.url)
+        fe.description(audio_file.description)
+        audio_file_path = os.path.join(base_url, audio_file.file.url)
+        file_size = os.path.getsize(audio_file.file.path)
+        fe.enclosure(audio_file_path, str(file_size), 'audio/mpeg')
+        fe.pubDate(audio_file.uploaded_at.strftime("%a, %d %b %Y %H:%M:%S +0000"))
+        fe.link(href=audio_file_path)
 
     fg.podcast.itunes_category('Technology', 'Podcasting')
     fg.podcast.itunes_image(base_url + '/path/to/your/podcast/image.jpg')
@@ -24,4 +27,6 @@ def create_rss_feed(audio_files, base_url):
     fg.podcast.itunes_author('Your Name')
     fg.podcast.itunes_explicit('no')
 
-    return fg.rss_str(pretty=True)
+    rss_feed_content = fg.rss_str(pretty=True)
+
+    return rss_feed_content
