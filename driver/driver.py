89e50c557999de6436ccee6e3bd5a87da0bc6279
riken-sajal <riken@sajaltech.com>
2024-08-05 19:14:05 +0530

    headless

--- driver/driver.py
+++ driver/driver.py
@@ -33,7 +33,7 @@ class Driver_class():
 
     def driver_args(self, Apple_profile):
         self.options = webdriver.ChromeOptions()
-        # self.options.add_argument("--headless")
+        self.options.add_argument("--headless")
         self.options.add_argument("--no-sandbox")
         self.options.add_argument("--disable-dev-shm-usage")
         self.options.add_argument("--disable-gpu")
