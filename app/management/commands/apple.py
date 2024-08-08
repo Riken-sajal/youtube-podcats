0d1743d9a04e577b2ca12be9cf1f7b4ad059b8d4
riken-sajal <riken@sajaltech.com>
2024-08-08 21:20:46 +0530

    update

--- app/management/commands/apple.py
+++ app/management/commands/apple.py
@@ -1,5 +1,5 @@
 # management/commands/upload_podcast_command.py
-
+import datetime, time
 from django.core.management.base import BaseCommand
 from driver.Upload_podcast import upload_podcast
 
@@ -8,5 +8,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         podcast_class = upload_podcast()
-        podcast_class.login_apple()
-        podcast_class.upload()
+        
+        # Initialize the start time for both tasks
+        last_login_time = datetime.datetime.now() - datetime.timedelta(minutes=5)
+        last_upload_time = datetime.datetime.now() - datetime.timedelta(hours=10)
+        first_upload = True
+        while True:
+            current_time = datetime.datetime.now()
+            
+            if (current_time - last_login_time).total_seconds() >= 5 * 60:
+                podcast_class.login_apple()
+                last_login_time = current_time
+            
+            if ((current_time - last_upload_time).total_seconds() >= 10 * 60 * 60) or first_upload:
+                podcast_class.upload()
+                last_upload_time = current_time
+            
+            # Sleep for a short period to prevent high CPU usage
+            time.sleep(1)
