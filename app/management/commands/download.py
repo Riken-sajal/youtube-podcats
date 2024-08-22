4cd8c788f5459da499f0894494600e614184879e
riken-sajal <riken@sajaltech.com>
2024-08-22 06:53:35 +0530

    dasdsadd

--- app/management/commands/download.py
+++ app/management/commands/download.py
@@ -20,21 +20,6 @@ def string_similarity(str1, str2):
     return similarity * 100
 
 
-    try:
-        # Run the `whoami` command using subprocess to get the current username
-        result = subprocess.run(['whoami'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        
-        # Check if the command was successful
-        if result.returncode == 0:
-            # Return the username, stripping any trailing whitespace or newline
-            return result.stdout.strip()
-        else:
-            # If the command failed, return the error message
-            return f"Error: {result.stderr.strip()}"
-    
-    except Exception as e:
-        # Handle any exceptions that may occur and return the exception message
-        return f"Exception occurred: {str(e)}"
     
 class Command(BaseCommand):
     help = 'Upload podcast'
--- driver/login_mail.py
+++ driver/login_mail.py
@@ -127,22 +127,7 @@ class Google(Driver_class):
                 
                 time.sleep(2)
         
-        def get_local_username():
-            try:
-                # Run the `whoami` command using subprocess to get the current username
-                result = subprocess.run(['whoami'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                
-                # Check if the command was successful
-                if result.returncode == 0:
-                    # Return the username, stripping any trailing whitespace or newline
-                    return result.stdout.strip()
-                else:
-                    # If the command failed, return the error message
-                    return f"Error: {result.stderr.strip()}"
-            
-            except Exception as e:
-                # Handle any exceptions that may occur and return the exception message
-                return f"Exception occurred: {str(e)}"
+        
             
             
         data = self.videos_data(video_url)
